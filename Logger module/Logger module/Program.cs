ac3747961edaf85a561dea1a231232af5c38aa06
KeeYoo <poronko2045@gmail.com>
2022-06-23 14:32:18 +0300

    Start commit

---
+++ Logger module/Logger module/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logger_module
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
